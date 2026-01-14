90b8499dbd460c20547e1ca0535ae8bfc82bd304
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2026-01-14 17:04:53 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -8,49 +8,116 @@ class Esercizio {
 
     //Elimina le componenti Verde e Blu, lasciando solo il Rosso
     public static void filtroRosso( int[][] R, int[][] G, int[][] B ) {
-        // TODO: Implementare il filtro
-        // Suggerimento: azzerare i valori di G e B per ogni pixel
+        for (int i = 0; i < R.length; ++i) {
+            for (int j = 0; j < R[0].length; ++j) {
+                G[i][j] = 0;
+                B[i][j] = 0;
+            }
+        }
     }
     
     //Bianco e nero: R, G, B impostati alla media dei valori
     public static void filtroBW( int[][] R, int[][] G, int[][] B ) {
-        // TODO: Implementare il filtro
-        // Suggerimento: calcolare la media di R, G, B e assegnarla a tutti e tre i canali
+        int media;
+        for (int i = 0; i < R.length; ++i) {
+            for (int j = 0; j < R[0].length; ++j) {
+                media = (R[i][j]+G[i][j]+B[i][j])/3;
+                R[i][j] = media;
+                G[i][j] = media;
+                B[i][j] = media;
+            }
+        }
     }
 
     //I pixel vicino al centro hanno più luminosità
     public static void filtroCentro(int[][] R, int[][] G, int[][] B) {
-        // TODO: Implementare il filtro
-        // Suggerimento: calcolare la distanza di ogni pixel dal centro
-        // e moltiplicare i valori RGB per un fattore proporzionale
+        double centroX, centroY, distanzaMax, distanza, fattore;
+        centroX = R.length/2;
+        centroY = R[0].length/2;
+        distanzaMax = Math.sqrt(Math.pow(centroX, 2) + Math.pow(centroY, 2));
+        for (int i = 0; i < R.length; ++i) {
+            for (int j = 0; j < R[0].length; ++j) {
+                distanza = Math.sqrt(Math.pow(i-centroX, 2) + Math.pow(j-centroY, 2));
+                fattore = Math.max(0, 1 - (distanza/distanzaMax));
+                R[i][j] *= fattore;
+                G[i][j] *= fattore;
+                B[i][j] *= fattore;
+            }
+        }
     }
 
     //Effetto glitch (rosso e blu sfasati)
     public static void filtroGlitch(int[][] R, int[][] G, int[][] B) {
-        // TODO: Implementare il filtro
-        // Suggerimento: creare copie di R e B, poi spostare i pixel
-        // rossi in alto a sinistra e i blu in basso a destra
+        int h = R.length;
+        int w = R[0].length;
+        int[][] R2 = new int[h][w];
+        int[][] B2 = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                R2[i][j] = R[i][j];
+                B2[i][j] = B[i][j];
+            }
+        }
+        azzeraMatrice(R);
+        azzeraMatrice(B);
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (i - 10 >= 0 && j - 10 >= 0) {
+                    R[i - 10][j - 10] = R2[i][j];
+                }
+                if (i + 10 < h && j + 10 < w) {
+                    B[i + 10][j + 10] = B2[i][j];
+                }
+            }
+        }
+}
+
+    public static void azzeraMatrice(int[][] M) {
+        for (int i = 0; i < M.length; ++i) {
+            for (int j = 0; j < M[0].length; ++j) {
+                M[i][j] = 0;
+            }
+        }
     }
 
     //Oscura i pixel che non sono Mare (prevalenza blu)
     public static void filtroMare(int[][] R, int[][] G, int[][] B) {
-        // TODO: Implementare il filtro
-        // Suggerimento: un pixel è "mare" se R<127, G<127, B>=127
-        // Oscurare (azzerare) i pixel che non soddisfano la condizione
+        for (int i = 0; i < R.length; ++i) {
+            for (int j = 0; j < R[0].length; ++j) {
+                if (!(R[i][j] < 127 && G[i][j] < 127 && B[i][j] >= 127)) {
+                    R[i][j] = 0; 
+                    G[i][j] = 0; 
+                    B[i][j] = 0; 
+                }
+            }
+        }
     }
 
     //Oscura i pixel che non sono Spiaggia (prevalenza giallo)
     public static void filtroSpiaggia(int[][] R, int[][] G, int[][] B) {
         // TODO: Implementare il filtro
-        // Suggerimento: un pixel è "spiaggia" se R>=127, G>=127, B<127
-        // Oscurare (azzerare) i pixel che non soddisfano la condizione
+        for (int i = 0; i < R.length; ++i) {
+            for (int j = 0; j < R[0].length; ++j) {
+                if (!(R[i][j]>=127 && G[i][j]>=127 && B[i][j]<127)) {
+                    R[i][j] = 0;
+                    G[i][j] = 0;
+                    B[i][j] = 0;
+                }
+            }
+        }
     }
 
     //Oscura i pixel che non sono Vegetazione (prevalenza verde)
     public static void filtroVegetazione(int[][] R, int[][] G, int[][] B) {
-        // TODO: Implementare il filtro
-        // Suggerimento: un pixel è "vegetazione" se R<100, G>=100, B<100
-        // Oscurare (azzerare) i pixel che non soddisfano la condizione
+        for (int i = 0; i < R.length; ++i) {
+            for (int j = 0; j < R[0].length; ++j) {
+                if (!(R[i][j]<100 && G[i][j]>=100 && B[i][j]<100)) {
+                    R[i][j] = 0;
+                    G[i][j] = 0;
+                    B[i][j] = 0;
+                }
+            }
+        }
     }
 
     
@@ -73,11 +140,11 @@ class Esercizio {
             System.out.println("Dimensione immagine: " + R.length+ "x" + R[0].length );
 
             //Chiama qui la funzione filtro, passando come parametri le matrici R,G,B da modificare, ad es.:
-            //filtroRosso(R, G, B);
+            filtroGlitch(R, G, B);
             //___
 
             //Assegna il nome del file di output (immagine con filtro), ad es. "faro_rosso.png":
-            String nomeFileOut = "___";
+            String nomeFileOut = "glitch2.png";
 
             ImageRGB.writeRGB(R, G, B, nomeFileOut);
  
